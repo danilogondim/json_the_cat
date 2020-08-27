3d23ec0ccf9cdaf9a6344693138bb88ffbc0cf35
Danilo Gondim <daniloencarnacao@gmail.com>
2020-08-27 18:26:40 +0000

    breedFetcher.js takes an argument from the CLI and requests the api url

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,10 @@
-const { request } = require('request');
+const request = require('request');
+
+const breed = process.argv[2];
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  if (error) {
+    throw error;
+  }
+  console.log(typeof body);
+})
