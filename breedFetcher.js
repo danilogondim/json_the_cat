ae92da51c30345a1e451b3ac02734ed6b48287e0
Danilo Gondim <daniloencarnacao@gmail.com>
2020-08-27 18:46:36 +0000

    Finished the breedFetcher.js. It accepts an argument from command line and handle edge cases properly

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,21 @@
 const request = require('request');
 
 const breed = process.argv[2];
+if (breed === undefined) {
+  console.log("Please provide a breed name.");
+  process.exit();
+}
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
   if (error) {
-    throw error;
+    console.log(error);
+    process.exit();
+    // throw error;
   }
-  console.log(typeof body);
-})
+  const data = JSON.parse(body);
+  if (data[0]) {
+    console.log(data[0].description);
+  } else {
+    console.log(`Breed not found.`);
+  }
+});
